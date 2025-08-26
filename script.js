cfdb40a8891c9fe0a8aca0dcf3edf58653ed08ad
clair <clair.theshruyb@gmail.com>
2025-08-26 11:03:30 -0400

    started working on the javascript/actual logic. made a function for create unique ids to store each capsule added a parse date function
    createCapsule funciton except its still empty

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 
+/* vars */
+const LOCAL_STORAGE_KEY = 'momentoCapsules';
 let capsules = [];
+let currentSelectedCapsuleId = null; // that is, year/month/day of selected
+let currentMonth = new Date().getMonth();
+let currentYear = new Date().getFullYear();
+const monthNames = ["january", "february", "march", "april", "may", 
+                    "june", "july", "august", "september", "october",
+                    "november", "december"];
+
+/* unique id to store items*/
+function generateUUID(){
+    return 'xxxxxxxx-xxxx-4xxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c){
+        var r = Math.random() * 16 | 0,
+            v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+function parseDate(date){
+    return new Intl.DateTimeFormat('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    }).format(date);
+}
 
 function createCapsule(){
-    
+
 }
