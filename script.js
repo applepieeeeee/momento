3a73024aaba24215a844a34ab27555f1c9845279
clair <clair.theshruyb@gmail.com>
2025-08-27 11:54:52 -0400

    debugging! fixed some typoes and ` written as ' :sob:

--- script.js
+++ script.js
@@ -32,12 +32,11 @@ function parseDate(date){
 
 function formatDateId(date){
     const d = new Date(date);
-
     const year = d.getFullYear();
     const month = (d.getMonth() + 1).toString().padStart(2, '0');
     const day = d.getDate().toString().padStart(2, '0');
 
-    return '${year}-${month}-${day}';
+    return `${year}-${month}-${day}`;
 }
 
 function normalizeDateToDay(date){
